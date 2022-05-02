d7af8d0099659b59019defcfc161bee24afcff9f
Erin Ewart <ems1980@hotmail.com>
2022-05-01 21:10:13 -0400

    created all connection queries to database

--- server.js
+++ server.js
@@ -1,150 +1,110 @@
-const inquirer = require ("inquirer")
+const inquirer = require("inquirer")
 const mysql = require('mysql2');
 const cTable = require('console.table');
-const connection = require("mysql2/typings/mysql/lib/Connection");
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-      host: '',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'Adv83839!',
-      database: 'election'
-    },
-    console.log('Connected to the employeecms database.')
-  );
+const connection = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Adv83839!',
+    database: 'employeecms'
+  });
+console.log('Connected to the employeecms database.');
+
 
 function whatToDo() {
-    return inquirer.prompt([
-        {
-            type:'list',
-            name:'todo',
-            message: 'What would you like to do?',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
-        }
-    ])
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'todo',
+      message: 'What would you like to do?',
+      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+    }
+  ])
     .then(answer => {
-      if (answer === 'View all departments') {
+      if (answer.todo === 'View all departments') {
         viewallDepartments()
-      } if (answer === 'View all roles') {
+      } if (answer.todo === 'View all roles') {
         viewAllRoles()
-      } if (answer === 'View all employees') {
+      } if (answer.todo === 'View all employees') {
         viewAllEmployees()
-      } if (answer === 'Add a department') {
+      } if (answer.todo === 'Add a department') {
         addDepartment()
-      } if (answer === 'Add a role') {
+      } if (answer.todo === 'Add a role') {
         addRole()
-      } if (answer === 'Add an employee') {
+      } if (answer.todo === 'Add an employee') {
         addEmployee()
-      } if (answer === 'Update an employee role') {
+      } if (answer.todo === 'Update an employee role') {
         updateEmployeeRole()
       }
     })
 };
 
 function viewallDepartments() {
-  connection.query(
-    `SELECT * FROM department;`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query('SELECT * FROM department', function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
 
 function viewAllRoles() {
-  connection.query(
-    `SELECT * FROM role`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query(`SELECT * FROM role`, function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
-
+ 
 function viewAllEmployees() {
-  connection.query(
-    `SELECT employee.first_name, employee.last_name role.salary, CONCAT(employee.first_name, ' ', employee.last_name) AS manager
-    FROM employee
-    LEFT JOIN employee manager on manager.id = employee.manager_id
-    INNER JOIN role ON (role.id = employee.role_id)
-    INNER JOIN department ON (department.id = role.department_id)
-    ORDER BY employee.id;`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query(`SELECT * FROM employee`, function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
+    
 
 function addDepartment() {
-  connection.query(
-    `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY role.title;`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query('INSERT INTO departments SET ?', department, function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
 
 function addRole() {
-  connection.query(
-    `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY role.title;`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query('INSERT INTO roles SET ?', role, function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
 
 function addEmployee() {
-  connection.query(
-    `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY role.title;`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query('INSERT INTO employees SET ?', employee, function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
 
 function updateEmployeeRole() {
-  connection.query(
-    `SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY role.title;`,
-    (err, res) => {
-    if (err) throw error;
-    console.table(res);
-    whatToDo();
-    }
-  )
+  connection.query('UPDATE employees SET ? where ?', [employee, {id: id}], function (error, results) {
+    if (error) {
+      console.log(err);
+    };
+    console.table(results);
+  });
 }
-
-
-
-
 
 
 whatToDo()
